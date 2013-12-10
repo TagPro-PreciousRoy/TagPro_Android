fd5d0e879c30736f406f808c74d63b04dab656c5
Jamie McClymont <jamie.mcclymont@gmail.com>
2013-12-11 10:04:46 +1300

    Automatically get the server list and display it
    Adds a few libraries, but some of them we will need later for socket.io
    anyway.

--- src/com/koalabeast/tagpro/MainMenuActivity.java
+++ src/com/koalabeast/tagpro/MainMenuActivity.java
@@ -1,15 +1,49 @@
 package com.koalabeast.tagpro;
 
-import android.os.Bundle;
+import android.app.ActionBar;
+import android.app.ActionBar.OnNavigationListener;
 import android.app.Activity;
+import android.os.Bundle;
 import android.view.Menu;
+import android.widget.ArrayAdapter;
 
-public class MainMenuActivity extends Activity {
+import com.google.gson.JsonArray;
+import com.google.gson.JsonElement;
+import com.google.gson.JsonObject;
+import com.koushikdutta.async.future.FutureCallback;
+import com.koushikdutta.ion.Ion;
 
-	@Override
+public class MainMenuActivity extends Activity implements OnNavigationListener {
+	private ArrayAdapter<String> serverListAdapter;
+
+	// @Overriden
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main_menu);
+
+		// Set up server list in the action bar
+		ActionBar bar = getActionBar();
+		serverListAdapter = new ArrayAdapter<String>(bar.getThemedContext(),
+				android.R.layout.simple_spinner_item);
+
+		serverListAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+		bar.setNavigationMode(ActionBar.NAVIGATION_MODE_LIST);
+		bar.setListNavigationCallbacks(serverListAdapter, this);
+
+		Ion.with(this, "http://tagpro.koalabeast.com/servers").asJsonArray()
+				.setCallback(new FutureCallback<JsonArray>() {
+
+					@Override
+					public void onCompleted(Exception e, JsonArray result) {
+
+						for (JsonElement serverElement : result) {
+							JsonObject server = serverElement.getAsJsonObject();
+							String serverName = server.get("name").getAsString();
+							serverListAdapter.add(serverName);
+						}
+					}
+
+				});
 	}
 
 	@Override
@@ -17,6 +51,12 @@ public class MainMenuActivity extends Activity {
 		// Inflate the menu; this adds items to the action bar if it is present.
 		getMenuInflater().inflate(R.menu.main_menu, menu);
 		return true;
+	}
+
+	@Override
+	public boolean onNavigationItemSelected(int itemPosition, long itemId) {
+		// TODO select server
+		return false;
 	}
 
 }
