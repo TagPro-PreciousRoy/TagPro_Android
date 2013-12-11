16cba5def39668c3161ec6c291fa9ea89d48b9da
Jamie McClymont <jamie.mcclymont@gmail.com>
2013-12-11 20:52:13 +1300

    Add music loop

--- src/com/koalabeast/tagpro/GameActivity.java
+++ src/com/koalabeast/tagpro/GameActivity.java
@@ -1,12 +1,14 @@
 package com.koalabeast.tagpro;
 
 import android.app.Activity;
+import android.media.MediaPlayer;
 import android.os.Bundle;
 import android.view.Window;
 import android.view.WindowManager;
 
 public class GameActivity extends Activity {
-
+	MediaPlayer mPlayer;
+	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
@@ -16,6 +18,24 @@ public class GameActivity extends Activity {
 		getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
 				WindowManager.LayoutParams.FLAG_FULLSCREEN);
 		setContentView(R.layout.activity_game);
+		
+		// Play music loop
+		// TODO make optional
+		mPlayer = MediaPlayer.create(this, R.raw.levelloop);
+		mPlayer.setLooping(true);
+		mPlayer.start();
+	}
+	
+	@Override
+	protected void onPause() {
+		super.onPause();
+		mPlayer.pause();
+	}
+	
+	@Override
+	protected void onResume() {
+		super.onResume();
+		mPlayer.start();
 	}
 
 }
