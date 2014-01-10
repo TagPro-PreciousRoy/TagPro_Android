2c66aa75a1f30e58f9a6a8121d5c2ba95f3984b3
roflmaoshizmp <rsedivy@gmail.com>
2014-01-10 22:28:46 +0100

    Added OpenGL Surface and Renderer
    Added OpenGL 2.0 into manifest
    GameActivity now has a GLSurface
    Created new GameRenderer class

--- src/com/koalabeast/tagpro/GameActivity.java
+++ src/com/koalabeast/tagpro/GameActivity.java
@@ -1,17 +1,28 @@
 package com.koalabeast.tagpro;
 
 import android.app.Activity;
+import android.content.Context;
 import android.media.MediaPlayer;
 import android.os.Bundle;
 import android.view.Window;
 import android.view.WindowManager;
+import android.opengl.*;
+
 
 public class GameActivity extends Activity {
 	MediaPlayer mPlayer;
+    private GLSurfaceView gameGLSview;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
+		
+        // Create a GLSurfaceView instance and set it
+        // as the ContentView for this Activity.
+        gameGLSview = new GameGLSurfaceView(this);
+        // Set the Renderer for drawing on the GLSurfaceView
+        gameGLSview.setRenderer(new GameRenderer());
+        setContentView(gameGLSview);
 
 		// Make fullscreen, hide action bar
 		requestWindowFeature(Window.FEATURE_NO_TITLE);
@@ -39,3 +50,19 @@ public class GameActivity extends Activity {
 	}
 
 }
+
+class GameGLSurfaceView extends GLSurfaceView {
+
+    public GameGLSurfaceView(Context context){
+        super(context);
+
+
+        
+        // Create an OpenGL ES 2.0 context
+        setEGLContextClientVersion(2);
+        
+        // Render the view only when there is a change in the drawing data
+        setRenderMode(GLSurfaceView.RENDERMODE_WHEN_DIRTY);
+    }
+}
+ 
